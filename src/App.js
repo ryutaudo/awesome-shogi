2bec713e6c4e80739bd9d5eabde9232676c9615b
Ryuta Udo <ryutaudo@gmail.com>
2018-11-08 23:46:25 +0900

    initial boardgame

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const TicTacToe = Game({
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell(G, ctx, id) {
+      let cells = [...G.cells]; // don't mutate original state.
+      cells[id] = ctx.currentPlayer;
+      return { ...G, cells }; // don't mutate original state.
+    },
+  },
+});
+
+const App = Client({ game: TicTacToe });
 
 export default App;
